d9db0d624c20b9113ae2c50fae579ab2ba268185
Anthony Snelling <anthony.snelling@live.com>
2021-07-03 13:01:09 -0400

    adding finished shooting gallery and work in progress TicTacToe

---
+++ ShootingGallery/ShootingGallery/Game1.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace ShootingGallery
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D targetSprite;
+        private Texture2D crosshairSprite;
+        private Texture2D backgroundSprite;
+        private SpriteFont gameFont;
+
+        private Vector2 targetPosition = new Vector2(915, 495);
+        private const int targetRadius = 45;
+
+        private MouseState mState;
+        private bool mReleased = true;
+        private int score = 0;
+
+        private double timer = 10;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = false;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            targetSprite = Content.Load<Texture2D>("gallery_assets/target");
+            crosshairSprite = Content.Load<Texture2D>("gallery_assets/crosshairs");
+            backgroundSprite = Content.Load<Texture2D>("gallery_assets/sky");
+            gameFont = Content.Load<SpriteFont>("gallery_assets/galleryFont");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if (timer > 0)
+            {
+                timer -= gameTime.ElapsedGameTime.TotalSeconds;
+                if (timer < 0)
+                {
+                    timer = 0;
+                }
+            }
+
+
+            mState = Mouse.GetState();
+
+            if (mState.LeftButton == ButtonState.Pressed && mReleased == true)
+            {
+                float mouseTargetDist = Vector2.Distance(targetPosition, mState.Position.ToVector2());
+
+                if (mouseTargetDist < targetRadius && timer > 0)
+                {
+                    score++;
+                    Random rand = new Random();
+                    targetPosition.X = rand.Next(10, _graphics.GraphicsDevice.Viewport.Width);
+                    targetPosition.Y = rand.Next(10, _graphics.GraphicsDevice.Viewport.Height - 90);
+                }
+
+                mReleased = false;
+            }
+
+            if (mState.LeftButton == ButtonState.Released)
+            {
+                mReleased = true;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
+            if (timer > 0)
+            {
+                _spriteBatch.Draw(targetSprite,
+                    new Vector2(targetPosition.X - targetRadius, targetPosition.Y - targetRadius), Color.White);
+            }
+
+            _spriteBatch.DrawString(gameFont, "Score: " + score.ToString(), new Vector2(3, 3), Color.White);
+            _spriteBatch.DrawString(gameFont, "Time: " + Math.Ceiling(timer).ToString(), new Vector2(3, 40),
+                Color.White);
+            if (timer == 0)
+            {
+                _spriteBatch.DrawString(gameFont, "GAMEOVER! " , new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2,  _graphics.GraphicsDevice.Viewport.Height / 2), Color.White);
+            }
+            _spriteBatch.Draw(crosshairSprite, new Vector2(mState.X - 25, mState.Y - 25), Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/TicTacToe/Game1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Threading;
+using System.Xml;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TicTacToe
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D gridSprite;
+        private Texture2D xSprite;
+        private Texture2D ySprite;
+
+        private Vector2 gridPos = new Vector2(0, 0);
+        private Vector2 gridScale = new Vector2(3, 3);
+        private Vector2 gridOrigin = new Vector2(0, 0);
+       Color defaultColor = Color.White; 
+       
+        private MouseState mState;
+
+        private int playerTurn = 1;
+        private int[,] gameBoard = new int[3,3];
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            
+        }
+
+        protected override void Initialize()
+        {
+            gridPos.X = (_graphics.GraphicsDevice.Viewport.Width) / 2;
+            gridPos.Y = (_graphics.GraphicsDevice.Viewport.Height) / 2;
+            
+            //initialize gameboard to all empty
+            foreach (int position in gameBoard)
+            {
+                gameBoard[position,position] = 0;
+                Console.Write(gameBoard[position,position] + ", ");
+            }
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            gridSprite = Content.Load<Texture2D>("Grid-TicTacToe");
+            xSprite = Content.Load<Texture2D>("X-TicTacToe");
+            ySprite = Content.Load<Texture2D>("O-TicTacToe");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            mState = Mouse.GetState();
+            
+            // TODO: Place pieces when click on empty spot
+            
+            // TODO: check and see if board has three in a row
+
+            //  alternate between pieces for player turns. 
+            if (playerTurn == 1)
+            {
+                playerTurn++;
+            }
+            else
+            {
+                playerTurn = 1;
+            } 
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            
+            Vector2 gridOffset = new Vector2(gridPos.X - 540, gridPos.Y - 540);
+            // TODO: Only Draw the X or O when you click a certain spot.
+
+            GraphicsDevice.Clear(Color.MidnightBlue);
+
+            _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp,
+                DepthStencilState.Default, RasterizerState.CullNone, null, null);
+
+            _spriteBatch.Draw(gridSprite, gridOffset,
+                null, defaultColor, 0,
+                gridOrigin, gridScale 
+                , SpriteEffects.None, 0);
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
